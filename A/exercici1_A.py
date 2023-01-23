a741b653445e8d0d21bd5a7cbdbc295424fb7d60
unknown <msoliz20@jaumebalmes.net>
2023-01-23 20:24:06 +0100

    Subido por Marco

---
+++ A/exercici1_A.py
@@ -0,0 +1,5 @@
+def añoNacimiento():
+    a = int(input("¿Cual es tu año de naminiento?"))
+    b = 2023 - a
+    print(f"Tienes {b} años")
+
---
+++ A/exercici2_A.py
@@ -0,0 +1,5 @@
+def diccionari():
+    myDict = {"Pedro":1, "Pablo":2, "Miguel":3, "Jose":4, "Angel":5, "Marco":6}
+    print(len(myDict))
+    print(myDict.values())
+    print(myDict.popitem())
